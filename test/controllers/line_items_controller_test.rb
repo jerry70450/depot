f82b974018941e16d1faf3fc4a85c03a6332bcdf
Jerry Chen <jerry.chen@shopify.com>
2019-06-25 14:11:56 -0400

    Iteration F3

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -56,4 +56,11 @@ assert_select 'td', "Programming Ruby 1.9"
 
     assert_redirected_to line_items_url
   end
+
+  test "should create line_item via ajax" do assert_difference('LineItem.count') do
+    post line_items_url, params: { product_id: products(:ruby).id },
+    xhr: true end
+    assert_response :success
+    assert_match /<tr class=\\"line-item-highlight/, @response.body 
+  end
 end
